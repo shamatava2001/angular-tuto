b191f53714bc855ecb0c673bbc4ea7c1a8af9db0
GiGson <shamatavagigucha@gmail.com>
2023-08-11 18:15:28 +0400

    service basics

--- src/app/directory/directory.component.ts
+++ src/app/directory/directory.component.ts
@@ -1,9 +1,11 @@
 import { Component} from '@angular/core';
+import { LoggingService } from '../logging.service';
 
 @Component({
   selector: 'app-directory',
   templateUrl: './directory.component.html',
   styleUrls: ['./directory.component.css'],
+  providers: [LoggingService]
 })
 
 export class DirectoryComponent {
@@ -12,7 +14,14 @@ export class DirectoryComponent {
     {name:'kakashi', belt:'red'},
     {name:'obito', belt:'yellow'}
   ]
-  
+
   term:any;
+  constructor(private logger: LoggingService){
+    console.log(logger);
+  }
+
+  logIt(){
+    this.logger.log();
+  }
 
 }
