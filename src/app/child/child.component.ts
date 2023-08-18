06cefb13cbc49aeb233694140f605c04e491e2ca
GiGson <shamatavagigucha@gmail.com>
2023-08-18 15:34:54 +0400

    ngOnInit

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnChanges, Input, SimpleChanges } from '@angular/core';
+import { Component, OnChanges, Input, SimpleChanges, OnInit, ViewChild, ElementRef, ContentChild } from '@angular/core';
 
 @Component({
   selector: 'app-child',
   templateUrl: './child.component.html',
   styleUrls: ['./child.component.css']
 })
-export class ChildComponent implements OnChanges {
+export class ChildComponent implements OnChanges, OnInit {
 
   @Input() textPar : string = 's';
   @Input() textPar2 : string = 's';
+  @ViewChild('par') paraEl : ElementRef | undefined;
+  @ContentChild('heading') headingEl : ElementRef | undefined;
 
   constructor(){
     console.log('constructor called,', this.textPar);
@@ -20,4 +22,10 @@ export class ChildComponent implements OnChanges {
     console.log('ngOnChanges called,', this.textPar, simpleChange);
   }
 
+  // 2. ngOnInit - როცა input property  ინიციალიზებულია.
+  // @viewChild, @contentChild მიუწვდომელია
+  ngOnInit(): void {
+    console.log('ngOnInit called.', `initial input values: ${this.textPar}, ${this.textPar2}`);
+    console.log(`in ngOnInit. @viewChild: ${this.paraEl}, @contentChild: ${this.headingEl}`);
+  }
 }
