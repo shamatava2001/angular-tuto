42ad565b77fad54e858083c5e6730b389dc2c596
GiGson <shamatavagigucha@gmail.com>
2023-08-18 23:21:16 +0400

    parent and child execution order

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, 
+  OnChanges, 
+  OnInit, 
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent{
+export class AppComponent implements OnChanges, OnInit, DoCheck, 
+AfterContentInit, AfterContentChecked, 
+AfterViewInit, AfterViewChecked{
+
   title = 'net-ninja';
   text = 'input1';
   text2 = 'input2'
@@ -22,5 +33,40 @@ export class AppComponent{
     this.show = !this.show;
   }
 
+  constructor(){
+    console.log(`parent constructor called`);
+  }
 
+  ngOnChanges(){
+    console.log('parent ngOnChanges called');
+  }
+
+
+  ngOnInit(): void {
+    console.log('parent ngOnInit called.');
+  }
+
+  ngDoCheck(): void {
+    console.log(`parent ngDoCheck called.`);
+  }
+
+  ngAfterContentInit(): void {
+    console.log(`parent ngAfterContentInit called.`);
+  }
+
+
+  ngAfterContentChecked(): void {
+    console.log(`parent ngAfterContentChecked called.`);
+  }
+
+  ngAfterViewInit(): void {
+    console.log(`parent ngAfterViewInit called.`);
+  }
+
+  ngAfterViewChecked(): void {
+    console.log(`parent ngAfterViewChecked called`);
+  }
+
+  
 }
+
