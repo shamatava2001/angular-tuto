597fd5a2f0f0a7e1ba63b62eb131716c68f2fa04
GiGson <shamatavagigucha@gmail.com>
2023-08-20 20:10:45 +0400

    setup routes and links

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { DirectoryComponent } from './directory/directory.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    DirectoryComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
