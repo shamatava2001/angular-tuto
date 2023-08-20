7b948e5a7ab7a37a8591dfa14c7abd58d5b17279
GiGson <shamatavagigucha@gmail.com>
2023-08-20 16:21:23 +0400

    custom prop & @input

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input} from '@angular/core';
+
 
 @Component({
   selector: 'app-home',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  @Input() text: string | undefined;
 
 }
