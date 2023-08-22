aa8ad8866eee869c3efec77bb7073309e52a149c
GiGson <shamatavagigucha@gmail.com>
2023-08-22 20:25:25 +0400

    simple observable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'net-ninja';
   name = 'giga';
   show : boolean = true;
@@ -18,6 +19,19 @@ export class AppComponent {
 
   handleCustomEvent(e:any){
     this.childData = e;
+  }
+
+  myObservable = new Observable((observer)=>{
+    console.log('observable starts');
+    observer.next('one');
+    observer.next('two');
+    observer.next('three');
+  });
+
+  ngOnInit(){
+    this.myObservable.subscribe((val)=>{
+      console.log(val);
+    });
   }
 
 }
