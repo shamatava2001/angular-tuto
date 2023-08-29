bdd35be2dbe6dc92c0c05424a799aaba21a652d8
GiGson <shamatavagigucha@gmail.com>
2023-08-29 19:23:53 +0400

    matDialog

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { MatDialog } from '@angular/material/dialog';
+import { DialogExampleComponent } from './dialog-example/dialog-example.component';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,5 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'net-ninja';
+  constructor(private dialog: MatDialog){ }
 
+  openDialog(){
+    const dialogRef = this.dialog.open(DialogExampleComponent, {data: {name: 'giga'} });
+
+    dialogRef.afterClosed().subscribe((res)=>{console.log(res)});
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material/material.module';
+import { DialogExampleComponent } from './dialog-example/dialog-example.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
+    DialogExampleComponent,
 
   ],
   imports: [
