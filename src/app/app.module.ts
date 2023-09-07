8f0ace4bb6c63de16e600b4472759cf7abbf7828
GiGson <shamatavagigucha@gmail.com>
2023-09-07 22:30:41 +0400

    HostListener

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { setBackgroundDirective } from './customDirectives/setBackground.directive';
+import { HighlightDirective } from './customDirectives/highlight.directive';
 
 @NgModule({
-  declarations: [AppComponent, setBackgroundDirective],
+  declarations: [AppComponent, setBackgroundDirective, HighlightDirective],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/customDirectives/highlight.directive.ts
@@ -0,0 +1,18 @@
+import { Directive, ElementRef, HostListener, Renderer2 } from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]',
+})
+export class HighlightDirective {
+  constructor(
+    private element: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  @HostListener('mouseenter') onMouseEnter() {
+    this.renderer.addClass(this.element.nativeElement, 'wrapper');
+  }
+  @HostListener('mouseout') onMouseOut() {
+    this.renderer.removeClass(this.element.nativeElement, 'wrapper');
+  }
+}
