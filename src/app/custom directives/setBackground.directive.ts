c25191f1fd669c166d39e534a4cd0243a8fe53e9
GiGson <shamatavagigucha@gmail.com>
2023-09-07 18:10:42 +0400

    Renderer2

--- src/app/custom directives/setBackground.directive.ts
+++ src/app/custom directives/setBackground.directive.ts
@@ -1,13 +1,22 @@
 import { Directive, ElementRef, OnInit } from '@angular/core';
+import { Renderer2 } from '@angular/core';
 
 @Directive({
   // metadata object
   selector: '[setBackground]',
 })
 export class setBackgroundDirective implements OnInit {
-  constructor(private element: ElementRef) {}
+  constructor(
+    private element: ElementRef,
+    private renderer: Renderer2
+  ) {}
 
   ngOnInit(): void {
-    this.element.nativeElement.style.backgroundColor = 'red';
+    // this.element.nativeElement.style.backgroundColor = 'red';
+    this.renderer.setStyle(
+      this.element.nativeElement,
+      'backgroundColor',
+      'red'
+    );
   }
 }
