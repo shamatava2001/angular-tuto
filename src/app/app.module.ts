bf7efc22a7b7c509742c459331182ee1340b79df
GiGson <shamatavagigucha@gmail.com>
2023-09-10 17:39:47 +0400

    re-creating ngclass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { setBackgroundDirective } from './customDirectives/setBackground.directive';
 import { HighlightDirective } from './customDirectives/highlight.directive';
 import { AppHoverDirective } from './customDirectives/appHover.directive';
+import { ClassDirective } from './customDirectives/class.directive';
 
 @NgModule({
   declarations: [
@@ -13,6 +14,7 @@ import { AppHoverDirective } from './customDirectives/appHover.directive';
     setBackgroundDirective,
     HighlightDirective,
     AppHoverDirective,
+    ClassDirective,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
---
+++ src/app/customDirectives/class.directive.ts
@@ -0,0 +1,23 @@
+import { Directive, ElementRef, Renderer2, Input } from '@angular/core';
+
+@Directive({
+  selector: '[appClass]',
+})
+export class ClassDirective {
+  constructor(
+    private element: ElementRef,
+    private renderer: Renderer2
+  ) {}
+
+  @Input() set appClass(value: Object) {
+    let entries = Object.entries(value);
+    for (let item of entries) {
+      let [className, condition] = item;
+      if (condition) {
+        this.renderer.addClass(this.element.nativeElement, className);
+      }
+    }
+
+    // console.log(entries);
+  }
+}
